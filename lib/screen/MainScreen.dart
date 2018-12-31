237e1706ca4c0d6b24cb5caa0a33c06a4e103c5b
Umesh Basnet <basnet0umesh@gmail.com>
2019-01-01 01:20:10 +0545

    * [x] main screen implementation

---
+++ lib/screen/MainScreen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
+  final pageTitle = ["Home", "Search", "Inbox"];
+  TabController tabController;
+
+  _MainScreenState() {
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          pageTitle[tabController.index],
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: Image.asset("image/app_icon.png"),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: <Widget>[
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  child: Text("Menu 1"),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: TabBarView(controller: tabController, children: [
+        Text(pageTitle[0]),
+        Text(pageTitle[1]),
+        Text(pageTitle[2])
+      ]),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: TabBar(
+            controller: tabController,
+            indicatorColor: Colors.transparent,
+            labelColor: Color(0xffFF5757),
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.home,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.search,
+                ),
+              ),
+              Tab(
+                icon: Icon(Icons.message),
+              )
+            ]),
+      ),
+    );
+  }
+}
