87c2856d399f77d701ac7bf8b463fb41e6c63d9c
Saroj Maharjan <maharjan.saroj93@gmail.com>
2019-01-08 22:50:17 +0545

    commented-sm

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -8,21 +8,21 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<FeedModel> feedList = List();
+  List<FeedModel> feedList = List();  //Declaring a list called 'feedList' of type FeedModel 
 
   @override
-  void initState() {
+  void initState() {      //initialising the state 
     // TODO: implement initState
-    super.initState();
-    feedList = FeedModel.getDummyData();
-    this.setState(() {});
+    super.initState();    // calling the parent initState method from StatefulWidget class
+    feedList = FeedModel.getDummyData();    //initialising our feedList with dummydata which is a static method that holds all the dummy details needed for screen
+    this.setState(() {}); //calling the setState method so as to notify the change made in the screen.
   }
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: feedList.length,
-        itemBuilder: (buildContext, position) {
+  Widget build(BuildContext context) {    //we must override a build method after extending a StatefulWidget class 
+    return ListView.builder(              //our build method returns a ListView, we are using a named Constructor of ListView.builder which lets us define our lists as per we require 
+        itemCount: feedList.length,       //itemCount and itemBuilder are two required parameters of ListView.builder constructor so we are giving the length of our list as given by our feedmodel.lenght
+        itemBuilder: (buildContext, position) { //itemBuilder lets us build our list visually which returns FeedItem with respective positions, feedlist[0], feedlist[1]... 
           return FeedItem(feedList[position]);
         });
   }
