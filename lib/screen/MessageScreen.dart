01b92be8934e97948aadf90abcbfd831d9055195
Saroj Maharjan <maharjan.saroj93@gmail.com>
2019-01-08 22:55:15 +0545

    commented-sm

--- lib/screen/MessageScreen.dart
+++ lib/screen/MessageScreen.dart
@@ -8,20 +8,20 @@ class MessageScreen extends StatefulWidget {
 }
 
 class _MessageScreenState extends State<MessageScreen> {
-  onNewMessagePress() {}
+  onNewMessagePress() {}    //???? a callback function with no any declaration
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          AppButton(
+    return Padding(   //With the help of Padding widget, specifying required spacing of 16dp from left, top, right bottom  
+      padding: const EdgeInsets.all(16.0),    
+      child: Column(    //
+        crossAxisAlignment: CrossAxisAlignment.stretch, //the crossaxis for Column means row wise so we are going to stretch our components row wise 
+        children: <Widget>[   //Column supports Multi Widgets so we are using that power to build a list of widget 
+          AppButton(        //AppButton is a custom button we designed so as to display a button as per our need which has text "New Message" passed via paramenter 
             "New Message",
-            this.onNewMessagePress,
+            this.onNewMessagePress, //this is the action we have to decide when the button is pressed
           ),
-          RecentMessage(),
+          RecentMessage(),      //???? 
         ],
       ),
     );
