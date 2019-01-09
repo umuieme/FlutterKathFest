532636a39085b69668990457c3d79b6e6f22c56b
Saroj Maharjan <maharjan.saroj93@gmail.com>
2019-01-09 08:31:17 +0545

    commented-sm

--- lib/model/FeedModel.dart
+++ lib/model/FeedModel.dart
@@ -1,21 +1,22 @@
 class FeedModel {
+  //declaring required variables for feeding into screen (Mock Dummy texts)
   final String name;
   final String description;
   final DateTime publishTime;
   final String image;
 
-  FeedModel(this.name, this.description, this.publishTime, this.image);
-
-  static List<FeedModel> getDummyData() {
-    final groupList = ["Flutter Nepal", "Google Developers Group", "Dubinu"];
-    final imageName = ["flutter-nepal.png", "gdg.png", "dubino.png"];
-    final description =
+  FeedModel(this.name, this.description, this.publishTime, this.image); //Constructor initialsing our instance variables 
+//static method lets us access the method without using any object & by using ClassName.staticMethod()
+  static List<FeedModel> getDummyData() {     //static method getDummyData which returns a List of FeedModel type
+    final groupList = ["Flutter Nepal", "Google Developers Group", "Dubinu"]; //list of group names
+    final imageName = ["flutter-nepal.png", "gdg.png", "dubino.png"];   //list of images available in our app
+    final description =     //a string of mock description
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e";
-    List<FeedModel> feedList = List<FeedModel>();
-    for (var i = 0; i < 20; i++) {
-      feedList.add(FeedModel(groupList[i % 3], description,
-          DateTime.now().subtract(Duration(hours: i * 2)), imageName[i % 3]));
+    List<FeedModel> feedList = List<FeedModel>();     //initialising a list of type FeedModel;
+    for (var i = 0; i < 20; i++) {                    //looping through 20 times
+      feedList.add(FeedModel(groupList[i % 3], description,     //adding items to our feedList,passing values to our constructor
+          DateTime.now().subtract(Duration(hours: i * 2)), imageName[i % 3]));  //i % 3 gives values from 0 , 1 and 2 which is required to loop throught our given mock list data
     }
-    return feedList;
+    return feedList;      //as declared above in method, we need to return a list of type FeedModel
   }
 }
